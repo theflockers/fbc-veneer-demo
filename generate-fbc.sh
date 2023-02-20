ef13965cb49d135fae1facdd355de7d29a59e278
Lance Galletti <lgallett@redhat.com>
2023-02-20 10:12:31 -0500

    composite

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
 # step0
-opm alpha render-veneer basic components/v4.13-step0.yaml > v4.13/catalog/catalog.yaml
-
-# step1
-# opm alpha render-veneer basic components/v4.13-step1.yaml > v4.13/catalog/catalog.yaml
-
-# step2
-# opm alpha render-veneer basic components/v4.13-step2.yaml > v4.13/catalog/catalog.yaml
-
-# step3
-# opm alpha render-veneer basic components/v4.13-step3.yaml > v4.13/catalog/catalog.yaml
+opm alpha render-veneer composite -f catalog-config.yaml -c composite.yaml --validate 
